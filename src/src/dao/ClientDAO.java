4771fa59b590062782a2b1a4ee436c0fa4edc23a
Dhia <Dhia@DESKTOP-ONGI1QN.mshome.net>
2018-11-05 10:41:15 +0100

    ClientDAO all crud works

--- src/src/dao/ClientDAO.java
+++ src/src/dao/ClientDAO.java
@@ -29,8 +29,6 @@ public class ClientDAO extends Initdao implements InterfaceDAO<Client> {
             st = cnx.connect().createStatement();
             rs = st.executeQuery(query);
 
-            int count = 0;
-
             while (rs.next()) {
                 c = new Client();
                 c.setCin(rs.getInt("cin"));
@@ -59,11 +57,11 @@ public class ClientDAO extends Initdao implements InterfaceDAO<Client> {
             pst.setString(4, e.getTelephone());
             pst.setString(5, e.getAdresse());
             rowsInserted = pst.executeUpdate();
-//            if (rowsInserted > 0) {
-//                System.out.println("Client ajouté avec succes!");
-//            } else {
-//                System.out.println("Erreur d'ajout");
-//            }
+            if (rowsInserted > 0) {
+                System.out.println("Client ajouté avec succes!");
+            } else {
+                System.out.println("Erreur d'ajout");
+            }
         } catch (SQLException ex) {
             //  ex.printStackTrace();
             System.out.println("Erreur d'ajout : verfifier les attributs du client");
@@ -74,31 +72,72 @@ public class ClientDAO extends Initdao implements InterfaceDAO<Client> {
 
     @Override
     public int delete(Client e) {
-         int rowsDeleted = 0;
+        int rowsDeleted = 0;
         try {
             query = "DELETE FROM client WHERE id=?";
             pst = cnx.connect().prepareStatement(query);
             pst.setInt(1, e.getId());
             rowsDeleted = pst.executeUpdate();
-//            if (rowsDeleted > 0) {
-//                System.out.println("A user was deleted successfully!");
-//            }
+            if (rowsDeleted > 0) {
+                System.out.println("A user was deleted successfully!");
+            }
         } catch (SQLException ex) {
-           ex.printStackTrace();
-           System.out.println("Erreur de suppr");
-           
+            ex.printStackTrace();
+            System.out.println("Erreur de suppr");
+
         }
         return rowsDeleted;
     }
 
     @Override
     public int update(Client e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int rowUpdated = 0;
+        try {
+            query = "UPDATE client SET cin=?,prenom=?, nom=?, telephone=?, adresse=?  WHERE id=?";
+            pst = cnx.connect().prepareStatement(query);
+            pst.setInt(1, e.getId());
+            pst.setInt(2, e.getCin());
+            pst.setString(3, e.getPrenom());
+            pst.setString(4, e.getNom());
+            pst.setString(5, e.getTelephone());
+            pst.setString(6, e.getAdresse());
+            rowUpdated = pst.executeUpdate();
+            if (rowUpdated > 0) {
+                System.out.println("A user was updated successfully!");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Erreur de mise à jour");
+
+        }
+        return rowUpdated;
+
     }
 
     @Override
-    public Client findById(int numero) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Client findById(int id) {
+
+        Client c = null;
+
+        try {
+            query = "SELECT * FROM client where id ="+id;
+            
+            st = cnx.connect().createStatement();
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+                c = new Client();
+                c.setId(rs.getInt("id"));
+                c.setCin(rs.getInt("cin"));
+                c.setPrenom(rs.getString("prenom"));
+                c.setNom(rs.getString("nom"));
+                c.setTelephone(rs.getString("telephone"));
+                c.setAdresse(rs.getString("adresse"));
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return c;
     }
 
 }
