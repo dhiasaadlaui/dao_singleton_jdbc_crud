d91e62a99c037a46f4286e1746c4095e0b963047
Dhia <Dhia@DESKTOP-ONGI1QN.mshome.net>
2018-11-05 12:22:02 +0100

    Adding Categorie entity

--- src/src/entities/Categorie.java
+++ src/src/entities/Categorie.java
@@ -5,10 +5,99 @@
  */
 package src.entities;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author Dhia
  */
 public class Categorie {
+   private int id;
+   private String libelle;
+   private String description;
+   private Categorie categorie;
+   
+   
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.id;
+        hash = 67 * hash + Objects.hashCode(this.libelle);
+        hash = 67 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categorie other = (Categorie) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.libelle, other.libelle)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" + "id=" + id + ", libelle=" + libelle + ", description=" + description + ", categorie=" + categorie + '}';
+    }
+
+    public Categorie() {
+    }
+
+    public Categorie(String libelle, String description, Categorie categorie) {
+        this.libelle = libelle;
+        this.description = description;
+        this.categorie = categorie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+   
+   
     
 }
